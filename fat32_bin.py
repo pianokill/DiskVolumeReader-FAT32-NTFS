8f1626bbe3d8a21d87df70adbfef653ec10dafc4
minh nhat <116612379+Diendien1801@users.noreply.github.com>
2024-02-23 12:08:49 +0700

    update read RDET

--- fat32_bin.py
+++ fat32_bin.py
@@ -12,9 +12,12 @@ class FAT32:
         self.sub_sector = ut.read_dec_offset(bootsector, 0x30, 2)  
         self.sector_store_bootsector = ut.read_dec_offset(bootsector, 0x32, 2)  
         self.fat_table = ""
+        
+        
         #FAT 32:
         #SECTORS IN BOOT SECTOR -> RESERVERD -> FAT TABLE -> ROOT DIRECTORY -> DATA AREA    
         self.rdet_sector_begin = self.n_sector_bootsector + self.n_fat_table * self.fat_table_size
+        
         self.data_sector_begin = self.rdet_sector_begin
     def print_bootsector(self):
         print("BOOT SECTOR INFORMATION")
@@ -30,24 +33,74 @@ class FAT32:
     def read_txt(self, buffer):
         data = ""
         return data
+    
     def read_entry(self, buffer):
-        name = 0
-        data = self.read_txt(buffer)
-        size = 0
-        return name, data, size
+        # thông tin entry : tên, phần mở rộng, tập tin or thư mục, kích thước, ? cluster bắt đầu ?
+        #tên
+        name = ut.read_bin_offset(buffer,0,8).decode('utf-8', errors='ignore').strip()
+        
+        #trạng thái
+        attr = ut.read_dec_offset(buffer,0xB,1)
+        
+
+        #attr = ut.describe_attr(attr)
+            
+
+        #cluster bắt đầu
+        cluster_begin = ut.read_dec_offset(buffer,0x1A,2)
+
+        #size
+        size = ut.read_dec_offset(buffer,0x1C,4)
+        return name, attr, cluster_begin, size
     def read_rdet(self, buffer):
         entries = []
+        sub_entries = []
+        buffer_subentry = ()
+        sub_name = ''
         for i in range(0, len(buffer), 32):
-            entry = self.read_entry(buffer[i:i+32])
-            entries.append(entry)
-        return entries
     
-file_fat32 = "/Users/pianokill/Documents/HCMUS-5th/Operating Systems/drive_fat.bin"
+            entry = self.read_entry(buffer[i:i+32])
+            entry += buffer_subentry
+            # chưa kết thúc tập tin
+            if(entry[1] != 15):
+       
+                buffer_subentry = ()
+            # có entry phụ trong tập tin
+                if(len(entry) > 4):
+                    # nối tên các entry phụ
+                    entry = list(entry)  
+                    entry[0] =""
+                    entry[0] += sub_name  
+                    entry = tuple(entry) 
+                    sub_name = '' 
+            
+                entries.append(entry)
+                sub_entries.clear()
+            else:
+                buffer_subentry += entry
+                sub_entries.append(buffer[i:i+32])
+                sub_entries.reverse()
+                sub_name += ut.process_fat_lfnentries(sub_entries)
+        return entries
+    def print_RDET(self,buffer):
+        print("FILE - DIRECTORY INFORMATION")
+        for i in range (0, len(buffer)):
+            if(buffer[i][1] ==0):
+                break
+            print("Name: ", buffer[i][0])
+            print("Attribute: ", ut.describe_attr(buffer[i][1]))
+            print("Cluster begin: ", buffer[i][2])
+            print("Size: ", buffer[i][3])
+    
+
+file_fat32 = "drive_fat.bin"
+
 bootsector_fat32 = ut.read_sector(file_fat32, 0, 1)
 volume_fat32 = FAT32(bootsector_fat32)     
-#volume_fat32.print_bootsector()
-
+volume_fat32.print_bootsector()
 RDET = ut.read_sector(file_fat32, volume_fat32.rdet_sector_begin, 1)
-print(RDET)
+entries = volume_fat32.read_rdet( RDET)
+volume_fat32.print_RDET(entries)
 
-#
+
+
--- utility.py
+++ utility.py
@@ -14,3 +14,35 @@ def read_bin_offset(buffer, offset, size):
 def read_dec_offset(buffer, offset, size):
     begin = buffer[offset:offset + size]
     return dec(begin[::-1].hex())
+def read_hex_offset(buffer, offset, size):
+    begin = buffer[offset:offset + size]
+    return begin.hex()
+def describe_attr(attr):
+        #tất cả thuộc tính
+        attributes = {
+            0x10: 'D',
+            0x20: 'A',
+            0x01: 'R', 
+            0x02: 'H',
+            0x04: 'S',
+        }
+        des = ''
+        for attribute in attributes:
+            if attr & attribute == attribute:
+                des += attributes[attribute]
+        
+        return des 
+def process_fat_lfnentries(subentries: list):
+        """
+        Hàm join các entry phụ lại thành tên dài
+        """
+        name = b''
+        for subentry in subentries:
+            name += read_bin_offset(subentry, 1, 10)
+            name += read_bin_offset(subentry, 0xE, 12)
+            name += read_bin_offset(subentry, 0x1C, 4)
+        name = name.decode('utf-16le', errors='ignore')
+
+        if name.find('\x00') > 0:
+            name = name[:name.find('\x00')]
+        return name
