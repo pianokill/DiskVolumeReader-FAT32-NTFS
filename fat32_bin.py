9374468221ff02a38e0d036fae8c13ca2bd50486
minh nhat <116612379+Diendien1801@users.noreply.github.com>
2024-02-27 11:48:12 +0700

    Update fat32_bin.py UI

--- fat32_bin.py
+++ fat32_bin.py
@@ -1,5 +1,5 @@
 import utility as ut
-
+import os
 class FAT32:     
     def __init__(self, path):
         self.path = path
@@ -17,16 +17,16 @@ class FAT32:
         self.rdet_sector_begin = self.n_sectors_bootsector + self.n_fat_tables * self.n_sectors_fat_table
         self.data_sector_begin = self.rdet_sector_begin
     def bootsector(self):
-        print("BOOT SECTOR INFORMATION OF ", self.path)
-        print("Sectors/cluster: ", self.n_sectors_cluster)
-        print("Sectors/boot sector: ", self.n_sectors_bootsector)
-        print("FAT Table: ", self.n_fat_tables)
-        print("Sectors in disk: ", self.volume_size)
-        print("Sectors in FAT table: ", self.n_sectors_fat_table)
-        print("RDET Cluster Begin: ", self.rdet_cluster_begin)
-        print("RDET Sector Begin: ", self.rdet_sector_begin)
-        print("Subsector: ", self.sub_sector)
-        print("Sector Store Boot sector: ", self.n_sectors_store_bootsector)
+        print("                         BOOT SECTOR INFORMATION OF", self.path)
+        print("      - Sectors/cluster: ", self.n_sectors_cluster)
+        print("      - Sectors/boot sector: ", self.n_sectors_bootsector)
+        print("      - FAT Table: ", self.n_fat_tables)
+        print("      - Sectors in disk: ", self.volume_size)
+        print("      - Sectors in FAT table: ", self.n_sectors_fat_table)
+        print("      - RDET Cluster Begin: ", self.rdet_cluster_begin)
+        print("      - RDET Sector Begin: ", self.rdet_sector_begin)
+        print("      - Subsector: ", self.sub_sector)
+        print("      - Sector Store Boot sector: ", self.n_sectors_store_bootsector)
     def cluster_to_sectors(self, cluster_n):
         sector_begin = self.n_sectors_bootsector + self.n_fat_tables*self.n_sectors_fat_table + (cluster_n - 2)*self.n_sectors_cluster
         sectors = [sector_begin]
@@ -54,7 +54,10 @@ class FAT32:
         cluster_begin = ut.read_dec_offset(buffer,0x1A,2)
         #size
         size = ut.read_dec_offset(buffer,0x1C,4)
-        return name, attr, cluster_begin, size  
+        #trạng thái
+        
+        e5 = ut.read_hex_offset(buffer, 0,1)
+        return name, attr, cluster_begin, size ,e5 
     def read_directory(self, entry):
             sectors = self.sectors_chain(entry[2])
             buffer = ut.read_list_sectors(self.path,sectors)
@@ -63,8 +66,11 @@ class FAT32:
             sub_entries = []
             buffer_subentry = ()
             sub_name = ''
+
+            
             for i in range(0, len(buffer), 32):
                 entry = self.read_entry(buffer[i:i+32])
+                
                 entry += buffer_subentry
                 # chưa kết thúc tập tin
                 if(entry[1] != 15):
@@ -79,7 +85,7 @@ class FAT32:
                         entry[0] += sub_name  
                         entry = tuple(entry) 
                         sub_name = '' 
-                    if(entry[1] != 0):
+                    if( entry[4] !='00' and entry[4] != 'e5'  ):
                         entries.append(entry)
                     sub_entries.clear()
                 else:
@@ -87,13 +93,14 @@ class FAT32:
                     sub_entries.append(buffer[i:i+32])
                     sub_entries.reverse()
                     sub_name += ut.process_fat_lfnentries(sub_entries)
+            
             return entries
     def print_text_file(self,path, entry):
         sectors = self.sectors_chain(entry[2])
         # đọc sector và decode utf8
         content_txt_file = ut.read_list_sectors(path, sectors)
         content_txt_file = content_txt_file[:entry[3]]
-        print(content_txt_file.decode('utf-8'))
+        print(content_txt_file.decode('utf-8', errors='ignore'))
     def print_directory(self,buffer):
         print("DIRECTORY INFORMATION")
         for i in range (0, len(buffer)):
@@ -101,15 +108,17 @@ class FAT32:
             print("Name: ", buffer[i][0])
             print("Attribute: ", ut.describe_attr(buffer[i][1]))
             print("Cluster begin: ", buffer[i][2])
-            print("Size: ", buffer[i][3])
+            print("Size: ", buffer[i][3])       
     def travel_to(self, path):
-        directories = path.split('/')
-        entry = ['rdet', 'D', self.rdet_cluster_begin, 0]
+        directories = path.split('\\')
+        
+        entry = ['rdet', 0, self.rdet_cluster_begin, 0]
         #Scan first directory in rdet
         for k in range(0, len(directories)):
             entries = self.read_directory(entry)
+            
             for i in range (0, len(entries)):
-                if directories[k] in entries[i][0]:
+                if directories[k] in entries[i][0]  :
                     entry[2] = entries[i][2]
                     entry[0] = entries[i][0]
                     entry[1] = entries[i][1]
@@ -124,18 +133,39 @@ class FAT32:
         entry = self.travel_to(path)
         print("PATH INFORMATION: ")
         print("NAME: ", entry[0])
-        print("ATTRIBUTE: ", ut.describe_attr(entry[1]))
+        if(ut.describe_attr(entry[1]) == 'D'):
+            print("ATTRIBUTE: Directory")
+        elif(ut.describe_attr(entry[1]) == 'A'):
+            print("ATTRIBUTE: File")
         print("CLUSTER BEGIN: ", entry[2])
         print("SIZE: ", entry[3])
+        
+        
+        apps = {
+        'pptx': 'PowerPoint',
+        'csv': 'Spreadsheet Software',
+        'json': 'Text Editor or JSON Viewer',
+        'pdf': 'PDF Reader',
+        'jpg': 'Image Viewer',
+        'mp3': 'Audio Player',
+        'mp4': 'Video Player',
+        'png': 'Photos'
+        # Thêm các định dạng file khác theo cần thiết
+        }
+        
         if ut.describe_attr(entry[1]) == 'D':
-            self.print_directory(self.read_directory(entry))
-        elif ut.describe_attr(entry[1]) == "A" and entry[3] != -1:
+            self.draw_tree(path)
+        elif ut.describe_attr(entry[1]) == "A" and entry[3] != -1 :
             self.print_text_file(self.path, entry)
         else:
-            print("ERROR")
+            file_extension = path.split('.')[-1].lower()
+           
+            suggested_application = apps.get(file_extension, 'Unknown app')
+            print("We currently do not support the functionality of reading this file. If you want to view the contents inside, you can use the following application: "+suggested_application )
     def draw_tree(self, path, indent = '', is_last=True):
         if path != 'rdet':
             entry_begin = self.travel_to(path)
+            
         else:
             entry_begin = ['rdet', 0x10, self.rdet_cluster_begin, '1']
         if ut.describe_attr(entry_begin[1]) != 'D':
@@ -165,17 +195,53 @@ class FAT32:
                     print("├── ", end='')
                 print(entry[0])
             i+=1
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+def main_screen():
+    
+    print("FAT32 AND NTFS EXPLORER PROJECT --  FIT HCMUS  --  22CLC07 \n")
+    print("----------------------------------------------------------")
+    print(" 22127222 - Nguyễn Thanh Tuấn Kiệt")
+    print(" 22127    - Nguyễn Minh Sơn")
+    print(" 22127068 - Trần Nguyễn Hoàng Diễn")
+    print("----------------------------------------------------------")
+    #path = input("Please provide your drive name: ")
+    path = "E"
+    path = r'\\.\\'+path+":"
+    drive = FAT32(path)
+    clear_screen() 
+    while True:
+        print("---------------------------------------------WORKING WITH DRIVE",path,"---------------------------------------------\n")
+        print("                                                 MENU\n") 
+        print("                                        1. Read bootsector")
+        print("                                        2. Print tree of directory")
+        print("                                        3. Display the content of a file.")
+        print("                                        4. Quit") 
+        choice = input("Please input your choice: ")
+        if choice == '1':
+            clear_screen()
+            drive.bootsector()
+        elif choice == '2':
+            directory = input("Please enter directory name: ")
+            drive.read_path(directory)
+            drive.draw_tree(directory)
+        elif choice == '3':
+            path_to_file = input("Please enter path to file: ")
+            drive.read_path(path_to_file.strip())
+        elif choice == '4':
+            print("GOODBYE!")
+            break
+        else:
+            print("Chọn không hợp lệ. Hãy chọn lại.")
 
-
-
-
-path = r'\\.\F:'
-drive = FAT32(path) 
-drive.bootsector()    
+#drive.bootsector()    
+main_screen()
 
 # rdet_entry = ['rdet', 0x10, drive.rdet_cluster_begin, 1]
 # entries_rdet = drive.read_directory(rdet_entry)
 # drive.print_directory(entries_rdet)
-
-drive.read_path('hoàng diễn')
-drive.draw_tree('hoàng diễn')
+#path = r'\\.\E:'
+#path = r'\\.\\'+path+":"
+##drive = FAT32(path)
+#drive.read_path("Hoàng Diễn")
+#drive.draw_tree('Hoàng Diễn')
